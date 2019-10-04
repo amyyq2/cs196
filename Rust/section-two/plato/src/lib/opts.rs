7f52a75cb52ae8e01031801b771ec0b5aeb40552
Amy Qian <amyyq2@illinois.edu>
2019-10-04 23:51:01 +0000

    opts.rs

--- Rust/section-two/plato/src/lib/opts.rs
+++ Rust/section-two/plato/src/lib/opts.rs
@@ -1,4 +1,20 @@
 use structopt::StructOpt;
 use std::path::PathBuf;
 
-// Fill me out
+#[derive(Debug, StructOpt)]
+#[structopt(name = "opts", about = "")]
+pub struct Opts {
+    #[structopt(short = "l")]
+    l: bool,
+    #[structopt(short = "h")]
+    h: bool,
+    #[structopt(short = "a")]
+    a: bool,
+    #[structopt(parse(from_os_str))]
+    directory: Option<PathBuf>,
+}
+
+fn main() {
+    let opt = Opts::from_args();
+    println!("{:?}", opt);
+}
