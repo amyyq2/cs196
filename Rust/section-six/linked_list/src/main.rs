78caf5eaf88b42db21e40d9c721088eb9b2b794a
Amy Qian <amyyq2@illinois.edu>
2019-10-30 16:00:09 +0000

    linked_list

--- Rust/section-six/linked_list/src/main.rs
+++ Rust/section-six/linked_list/src/main.rs
@@ -1,26 +1,25 @@
 #[derive(Debug)]
 struct Node<T> {
-    next: ?,
+    next: Option<Box<Node<T>>>,
     value: T
 }
 
 impl<T> Node<T> {
-    fn set_next(&mut self, ?) {
-        self.next = ?;
+    fn set_next(&mut self, element: Node<T>) {
+        self.next = Some(Box::new(element));
     }
 }
 
 fn main() {
     let mut head = Node {
-        next: ?,
+        next: None,
         value: 1
     };
     
     let next = Node {
-        next: ?,
+        next: None,
         value: 2
     };
-    
     head.set_next(next);
     
     println!("{:?}", head);
