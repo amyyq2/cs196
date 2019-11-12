14c9c4758737e8e9c076ca15bf0d92cedd0f9b0e
Amy Qian <amyyq2@illinois.edu>
2019-11-12 02:28:57 +0000

    mp

--- Haskell/haskell-mp-part1/src/Lib.hs
+++ Haskell/haskell-mp-part1/src/Lib.hs
@@ -3,12 +3,14 @@ import Data.Char
 import Data.List
 
 shift :: Char -> Int -> Char
-shift l n = undefined
+shift l n | isLower l = chr $ ((ord l - ord 'a' + n) `mod` 26) + ord 'a'
+	  | isUpper l = chr $ ((ord l - ord 'A' + n) `mod` 26) + ord 'A'
+	  | otherwise = l
 
 cipherEncode :: String -> Int -> String
-cipherEncode ss n = undefined
+cipherEncode ss n = [shift l n | l <- ss]
 
 cipherDecode :: String -> Int -> String
-cipherDecode ss n = undefined
+cipherDecode ss n = [shift l (- n) | l <- ss]
 
 -- Feel free to add any helper functions
