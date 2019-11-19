ebdce5ac21be04c86a27b75a5545f9a961627edf
Amy Qian <amyyq2@illinois.edu>
2019-11-19 17:01:30 +0000

    lecture_d

--- Haskell/section-five/lecture_activity_d.hs
+++ Haskell/section-five/lecture_activity_d.hs
@@ -7,4 +7,12 @@ import Data.Char
 --- to test this function, create a "main" function, compile, and run `cat tweet.txt | lecture_activity_d.hs`
 
 newGetLine :: IO String
-newGetLine = undefined
+newGetLine = do 
+    c <- getChar
+    if c == '\n' then return ""
+    else
+	do
+	    s <- getLine
+	    return (c:s)
+
+main = undefined
