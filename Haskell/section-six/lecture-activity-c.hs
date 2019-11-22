0652b0ff6f804acf62691ecbd05d7e729db15ea9
Amy Qian <amyyq2@illinois.edu>
2019-11-22 05:54:18 +0000

    lecture_c

--- Haskell/section-six/lecture-activity-c.hs
+++ Haskell/section-six/lecture-activity-c.hs
@@ -6,4 +6,25 @@ You can test this by first compiling, then running "./lecture-activity-c"
 
 import Control.Concurrent
 
-main = undefined
+zero = do
+    putStrLn "zero"
+    zero
+
+one = do
+    putStrLn "one"
+    one
+
+checkInput :: String -> Bool
+checkInput input 
+    | input == "QUIT" = True
+    | otherwise = False
+
+main = do
+    putStrLn "Baginning Transmission: "
+    forkIO $ (zero)
+    forkIO $ (one)
+    exit <- getLine
+    if checkInput exit
+	then putStrLn "Thank you for using InfiniteCodeUIUC (TM)!"
+	else main
+
